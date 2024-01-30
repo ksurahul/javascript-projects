ff00e62e70a3d4071f41d7692a289f657066dc4b
unknown <rahulpksu@gmail.com>
2024-01-29 21:23:25 -0600

    part one done

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -6,5 +6,35 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
+// let numArray = function (arr){
+//     let min = 0;
 
-console.log(/* your code here */);
+//     for(i = 0; i < arr.length; i++){
+
+//         if(arr[i] < arr[i+1]){
+//             min = arr[i];
+//             console.log(min);
+//         }
+
+//     }
+//     return min
+// }
+
+// assume that nums has atleast one number in the array
+// assume that there can be negative numbers
+function numArray(nums) {
+    let min = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (num < min) {
+            min = num;
+        }
+    }
+
+    return min;
+}
+
+let min = numArray(nums1);
+console.log(min);
