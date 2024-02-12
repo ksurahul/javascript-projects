04b23ce6df8087ef911ffc073bb1aad981d1f172
unknown <rahulpksu@gmail.com>
2024-02-12 16:10:08 -0600

    complete

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,13 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+
+function divde (numerator, denominator) {
+    if (denominator === 0) {
+        throw Error('You cannot divde by 0!')
+    }
+    return numerator / denominator;
+}
+
+console.log(divde(1,4));
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,53 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    try {
+      let result = lab.runLab(3);     
+    } catch(error) {
+      return 'Error thrown'
+    }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
 
-let studentLabs = [
+// let studentLabs = [
+//   {
+//     student: 'Carly',
+//     runLab: function (num) {
+//         return Math.pow(num, num);
+//     }
+//   },
+//   {
+//     student: 'Erica',
+//     runLab: function (num) {
+//         return num * num;
+//     }
+//   }
+// ];
+
+// gradeLabs(studentLabs);
+
+
+
+let studentLabs2 = [
   {
-    student: 'Carly',
-    runLab: function (num) {
+      student: 'Blake',
+      myCode: function (num) {
         return Math.pow(num, num);
-    }
+      }
   },
   {
-    student: 'Erica',
-    runLab: function (num) {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
         return num * num;
-    }
+      }
   }
 ];
 
-gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
